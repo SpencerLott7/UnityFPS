67018a47bcf3e80ee299f1604417396a7649c54f
SpencerLott7 <Spencerlott136@u.boisestate.edu>
2024-01-30 15:43:56 -0700

    Color

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -21,6 +21,7 @@ public class SceneController : MonoBehaviour
             enemy = Instantiate(enemyPrefab) as GameObject;
             enemy.transform.position = new Vector3(0, 1f, 0);
             enemy.transform.localScale = new Vector3(1,Random.Range(2f, 6f),1);
+            enemy.GetComponent<MeshRenderer>().material.color=new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f),1);
             //rend.material.Color(Random.Range(0,1),Random.Range(0,1),Random.Range(0,1));
             float angle = Random.Range(0, 360);
             enemy.transform.Rotate(0, angle, 0);
